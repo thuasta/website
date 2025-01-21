c159fc2dc1915229d90df2b36c8ea8d90ad1ad9a
Yunyang <sunnycloudyang@outlook.com>
2025-01-22 00:02:43 +0800

    fix: fix for ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import SplitText from '../components/SplitText';
-import PixelCard from '../components/PixelCard';
+import SplitText from '@site/src/components/SplitText';
+import PixelCard from '@site/src/components/PixelCard';
 import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
@@ -14,35 +15,39 @@ import React, { JSX } from 'react';
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <PixelCard className={styles.heroBanner} gap={undefined} speed={undefined} colors={undefined} noFocus={undefined}>
-      <div className="container" style={{position: "absolute"}}>
-        <Heading as='h1'>
-          <SplitText
-            text="自动化系学生科协"
-            className="text-center hero__title"
-            delay={50}
-            animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
-            animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
-            easing="easeOutCubic"
-            threshold={0.2}
-            rootMargin="-50px"
-            onLetterAnimationComplete={()=>{}}
-          />
-        </Heading>
-        <SplitText
-          text="welcome to our website!"
-          className="text-center"
-          delay={10}
-          offset={50 * 8 + 50}
-          animationFrom={{ opacity: 0, transform: 'translate3d(0,20px,0)' }}
-          animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
-          easing="easeOutCubic"
-          threshold={0.2}
-          rootMargin="-50px"
-          onLetterAnimationComplete={()=>{}}
-        />
-      </div>
-    </PixelCard>
+    <BrowserOnly>
+      {() => 
+        <PixelCard className={styles.heroBanner} gap={undefined} speed={undefined} colors={undefined} noFocus={undefined}>
+          <div className="container" style={{position: "absolute"}}>
+            <Heading as='h1'>
+              <SplitText
+                text="自动化系学生科协"
+                className="text-center hero__title"
+                delay={50}
+                animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+                animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+                easing="easeOutCubic"
+                threshold={0.2}
+                rootMargin="-50px"
+                onLetterAnimationComplete={()=>{}}
+              />
+            </Heading>
+            <SplitText
+              text="welcome to our website!"
+              className="text-center"
+              delay={10}
+              offset={50 * 8 + 50}
+              animationFrom={{ opacity: 0, transform: 'translate3d(0,20px,0)' }}
+              animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+              easing="easeOutCubic"
+              threshold={0.2}
+              rootMargin="-50px"
+              onLetterAnimationComplete={()=>{}}
+            />
+          </div>
+        </PixelCard>
+      }
+    </BrowserOnly>
   );
 }
 
