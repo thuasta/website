65283645b5087620f8c4f1d28aa70ba7756941bd
Yunyang <sunnycloudyang@outlook.com>
2025-01-21 23:36:33 +0800

    feat: adjust banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,46 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import SplitText from '../components/SplitText';
+import PixelCard from '../components/PixelCard';
 import Heading from '@theme/Heading';
 
-import styles from './index.module.css'; // 引入局部样式
+import styles from './index.module.css';
 
 import React, { JSX } from 'react';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          自动化系学生科协
+    <PixelCard className={styles.heroBanner} gap={undefined} speed={undefined} colors={undefined} noFocus={undefined}>
+      <div className="container" style={{position: "absolute"}}>
+        <Heading as='h2'>
+          <SplitText
+            text="自动化系学生科协"
+            className="text-center hero__title"
+            delay={50}
+            animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+            animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+            easing="easeOutCubic"
+            threshold={0.2}
+            rootMargin="-50px"
+            onLetterAnimationComplete={()=>{}}
+          />
         </Heading>
-        <p className={styles.neonSubtitle}>
-          welcome to our website!
-        </p>
+        <SplitText
+          text="welcome to our website!"
+          className="text-center"
+          delay={10}
+          offset={50 * 8 + 50}
+          animationFrom={{ opacity: 0, transform: 'translate3d(0,20px,0)' }}
+          animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+          easing="easeOutCubic"
+          threshold={0.2}
+          rootMargin="-50px"
+          onLetterAnimationComplete={()=>{}}
+        />
       </div>
-    </header>
+    </PixelCard>
   );
 }
 
